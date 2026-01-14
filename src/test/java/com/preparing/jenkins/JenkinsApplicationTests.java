13d6e7a025320dae1312d34a146dc5620b11b27a
Salman <salman.b@sumedhastech.com>
2026-01-14 15:43:50 +0530

    First commit

---
+++ src/test/java/com/preparing/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,25 @@
+package com.preparing.jenkins;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class JenkinsApplicationTests {
+
+    public static Logger logger = LoggerFactory.getLogger(JenkinsApplicationTests.class);
+
+
+    @Test
+	void contextLoads() {
+        logger.info("Test case executing...");
+        Assertions.assertEquals(true,true);
+    }
+
+
+}
